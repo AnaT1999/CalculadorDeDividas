6f5646b14e050bb5bdc3b19e68c614143641566b
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-06-17 02:03:00 +0100

    layout e inserirNumeros individuais + problema na inserção de dados 17/06/2024

--- app/src/main/java/pt/ipg/calculadordedividas/MainActivity.kt
+++ app/src/main/java/pt/ipg/calculadordedividas/MainActivity.kt
@@ -1,9 +1,11 @@
 package pt.ipg.calculadordedividas
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -22,6 +24,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,9 +38,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import pt.ipg.calculadordedividas.ui.theme.CalculadorDeDividasTheme
 import java.text.NumberFormat
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -59,11 +67,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun InserirNumeros(@StringRes label: Int,
                    keyboardOptions: KeyboardOptions,
-                   value: String,
+                   value: MutableState<String>,
                    onValueChanged: (String) -> Unit,
                    modifier: Modifier = Modifier) {
 
-    var DividaInp by remember {mutableStateOf("")}
+    /*var DividaInp by remember {mutableStateOf("")}
     val Divida = DividaInp.toDoubleOrNull() ?: 0.0
 
     var EmprestimoInp by remember { mutableStateOf("") }
@@ -78,16 +86,201 @@ fun InserirNumeros(@StringRes label: Int,
     var PercentOInp by remember { mutableStateOf("") }
     val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
 
-    val JurosT = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
     val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
     val resto = calcularResto(Emprestimo, Divida)
 
    // val mes = CalcularTempoMes(orcamento, JurosT, resto)
    // val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+   */
+
 
     TextField(
-        value = value,
-        onValueChange = onValueChanged,
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            onValueChanged(it)
+        },
+        singleLine = true,
+        label = { Text(stringResource(label)) },
+        modifier = modifier,
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun InserirNumeros2(@StringRes label: Int,
+                   keyboardOptions: KeyboardOptions,
+                    value: MutableState<String>,
+                    onValueChanged: (String) -> Unit,
+                   modifier: Modifier = Modifier) {
+
+    /*var DividaInp by remember {mutableStateOf("")}
+    val Divida = DividaInp.toDoubleOrNull() ?: 0.0
+
+    var EmprestimoInp by remember { mutableStateOf("") }
+    val Emprestimo = EmprestimoInp.toDoubleOrNull() ?: 0.0
+
+    var JurosPerctInp by remember { mutableStateOf("") }
+    val JurosPerct = JurosPerctInp.toDoubleOrNull() ?: 0.0
+
+    var ordenadoInp by remember { mutableStateOf("") }
+    val ordenado = ordenadoInp.toDoubleOrNull() ?: 0.0
+
+    var PercentOInp by remember { mutableStateOf("") }
+    val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
+
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
+    val resto = calcularResto(Emprestimo, Divida)
+
+   // val mes = CalcularTempoMes(orcamento, JurosT, resto)
+   // val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+   */
+
+
+    TextField(
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            onValueChanged(it)
+        },
+        singleLine = true,
+        label = { Text(stringResource(label)) },
+        modifier = modifier,
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun InserirNumeros3(@StringRes label: Int,
+                   keyboardOptions: KeyboardOptions,
+                    value: MutableState<String>,
+                    onValueChanged: (String) -> Unit,
+                   modifier: Modifier = Modifier) {
+
+    /*var DividaInp by remember {mutableStateOf("")}
+    val Divida = DividaInp.toDoubleOrNull() ?: 0.0
+
+    var EmprestimoInp by remember { mutableStateOf("") }
+    val Emprestimo = EmprestimoInp.toDoubleOrNull() ?: 0.0
+
+    var JurosPerctInp by remember { mutableStateOf("") }
+    val JurosPerct = JurosPerctInp.toDoubleOrNull() ?: 0.0
+
+    var ordenadoInp by remember { mutableStateOf("") }
+    val ordenado = ordenadoInp.toDoubleOrNull() ?: 0.0
+
+    var PercentOInp by remember { mutableStateOf("") }
+    val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
+
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
+    val resto = calcularResto(Emprestimo, Divida)
+
+   // val mes = CalcularTempoMes(orcamento, JurosT, resto)
+   // val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+   */
+
+
+    TextField(
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            onValueChanged(it)
+        },
+        singleLine = true,
+        label = { Text(stringResource(label)) },
+        modifier = modifier,
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun InserirNumeros4(@StringRes label: Int,
+                   keyboardOptions: KeyboardOptions,
+                    value: MutableState<String>,
+                    onValueChanged: (String) -> Unit,
+                   modifier: Modifier = Modifier) {
+
+    /*var DividaInp by remember {mutableStateOf("")}
+    val Divida = DividaInp.toDoubleOrNull() ?: 0.0
+
+    var EmprestimoInp by remember { mutableStateOf("") }
+    val Emprestimo = EmprestimoInp.toDoubleOrNull() ?: 0.0
+
+    var JurosPerctInp by remember { mutableStateOf("") }
+    val JurosPerct = JurosPerctInp.toDoubleOrNull() ?: 0.0
+
+    var ordenadoInp by remember { mutableStateOf("") }
+    val ordenado = ordenadoInp.toDoubleOrNull() ?: 0.0
+
+    var PercentOInp by remember { mutableStateOf("") }
+    val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
+
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
+    val resto = calcularResto(Emprestimo, Divida)
+
+   // val mes = CalcularTempoMes(orcamento, JurosT, resto)
+   // val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+   */
+
+
+    TextField(
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            onValueChanged(it)
+        },
+        singleLine = true,
+        label = { Text(stringResource(label)) },
+        modifier = modifier,
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun InserirNumeros5(@StringRes label: Int,
+                   keyboardOptions: KeyboardOptions,
+                    value: MutableState<String>,
+                    onValueChanged: (String) -> Unit,
+                   modifier: Modifier = Modifier) {
+
+    /*var DividaInp by remember {mutableStateOf("")}
+    val Divida = DividaInp.toDoubleOrNull() ?: 0.0
+
+    var EmprestimoInp by remember { mutableStateOf("") }
+    val Emprestimo = EmprestimoInp.toDoubleOrNull() ?: 0.0
+
+    var JurosPerctInp by remember { mutableStateOf("") }
+    val JurosPerct = JurosPerctInp.toDoubleOrNull() ?: 0.0
+
+    var ordenadoInp by remember { mutableStateOf("") }
+    val ordenado = ordenadoInp.toDoubleOrNull() ?: 0.0
+
+    var PercentOInp by remember { mutableStateOf("") }
+    val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
+
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
+    val resto = calcularResto(Emprestimo, Divida)
+
+   // val mes = CalcularTempoMes(orcamento, JurosT, resto)
+   // val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+   */
+
+
+    TextField(
+        value = value.value,
+        onValueChange = {
+            value.value = it
+            onValueChanged(it)
+        },
         singleLine = true,
         label = { Text(stringResource(label)) },
         modifier = modifier,
@@ -98,8 +291,8 @@ fun InserirNumeros(@StringRes label: Int,
 
 
 
-
-
+@SuppressLint("UnrememberedMutableState")
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun LayoutCalculadorDividas() {
     var DividaInp by remember {mutableStateOf("")}
@@ -117,14 +310,23 @@ fun LayoutCalculadorDividas() {
     var PercentOInp by remember { mutableStateOf("") }
     val PercentO = PercentOInp.toDoubleOrNull() ?: 0.0
 
-    val JurosT = calcularJurosEmprestimo(Emprestimo, JurosPerct)
+    val juros_total = calcularJurosEmprestimo(Emprestimo, JurosPerct)
     val orcamento = calcularPercentagemOrdenado(ordenado, PercentO)
     val resto = calcularResto(Emprestimo, Divida)
 
-    val mes = CalcularTempoMes(orcamento, JurosT, resto)
-    val semanas = CalcularTempoSemanas(orcamento, JurosT, resto)
+    //Passar parametros para Double para facilitar a conta
+    val jurosTD = NumberFormat.getCurrencyInstance().parse(juros_total)?.toDouble() ?: 0.0
+    val orcamentoD = NumberFormat.getCurrencyInstance().parse(orcamento)?.toDouble() ?: 0.0
+    val restoD = NumberFormat.getCurrencyInstance().parse(resto)?.toDouble() ?: 0.0
+
+    //Atribuir aos valores meses e semanas os valores
+    val mes = calcularTempoMes(orcamentoD, jurosTD, restoD)
+    val semanas = calcularTempoSemanas(orcamentoD, jurosTD, restoD)
 
 
+    val TamanhoText = TextStyle(
+        fontSize = 26.sp
+    )
 
 
     Column (modifier = Modifier
@@ -136,51 +338,59 @@ fun LayoutCalculadorDividas() {
 
              Text(text = stringResource(R.string.calcula_div),modifier = Modifier
              .padding(bottom = 16.dp, top = 40.dp)
-             .align(alignment = Alignment.Start) )
+             .align(Alignment.CenterHorizontally), style = TamanhoText )
 
-        //Emprestimo
+        //Inserir valor de Emprestimo
             InserirNumeros( label = R.string.emp,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
-                value = EmprestimoInp,
+                value = mutableStateOf(EmprestimoInp) ,
                 onValueChanged = { EmprestimoInp = it },
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
-        //Divida
-            InserirNumeros( label = R.string.div,
+
+
+        //Inserir valor de Divida
+            InserirNumeros2( label = R.string.div,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
-                value = DividaInp,
+                value =  mutableStateOf(DividaInp),
                 onValueChanged = { DividaInp = it },
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
-        //Percentagem de juros sobre o emprestimo
-            InserirNumeros( label = R.string.empjuros,
+
+        //Inserir valor de Percentagem de juros sobre o emprestimo
+            InserirNumeros3( label = R.string.empjuros,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
-                value = JurosPerctInp,
+                value = mutableStateOf(JurosPerctInp),
                 onValueChanged = { JurosPerctInp = it },
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
-        //Ordenado
-            InserirNumeros( label = R.string.ord,
+        //Inserir valor de Ordenado
+            InserirNumeros4( label = R.string.ord,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Next),
-                value = ordenadoInp,
+                value = mutableStateOf(ordenadoInp),
                 onValueChanged = { ordenadoInp = it },
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
-        //Valor usado do ordenado (Percentagem)
-            InserirNumeros( label = R.string.percentord,
+
+        //Inserir valor usado do ordenado (Percentagem)
+            InserirNumeros5( label = R.string.percentord,
                 keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
-                value = PercentOInp,
+                value = mutableStateOf(PercentOInp),
                 onValueChanged = { PercentOInp = it },
                 modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
 
 
+
         Text(text = stringResource(R.string.mesesdiv, mes),
-            style = MaterialTheme.typography.displaySmall
+            style = TamanhoText
         )
-        Spacer(modifier = Modifier.height(150.dp))
+
+        Spacer(modifier = Modifier.height(45.dp))
         Text(text = stringResource(R.string.semanadiv, semanas),
-            style = MaterialTheme.typography.displaySmall
+            style = TamanhoText
         )
+
+        Spacer(modifier = Modifier.height(150.dp))
 
     }
 }
@@ -204,7 +414,7 @@ private fun calcularResto(Emprestimo: Double, Divida: Double ): String {
     val resto = Emprestimo - Divida
     return NumberFormat.getCurrencyInstance().format(resto)
 }
-private fun calcularTempoMes(orcamento: String, jurosT: String, resto: String ): Int {
+private fun calcularTempoMes(orcamento: Double, jurosT: Double ,resto: Double): Int {
     var mes = 0
     var DividaRestante = jurosT - resto
 
@@ -215,7 +425,7 @@ private fun calcularTempoMes(orcamento: String, jurosT: String, resto: String ):
     return mes
 }
 
-private fun calcularTempoSemanas(orcamento: String, jurosT: String ,resto: String): Int {
+private fun calcularTempoSemanas(orcamento: Double, jurosT: Double ,resto: Double): Int {
     var semanas = 0
     var DividaRestante = jurosT - resto
 
@@ -227,6 +437,7 @@ private fun calcularTempoSemanas(orcamento: String, jurosT: String ,resto: Strin
 }
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun LayoutCalculadorDividasPreview() {
